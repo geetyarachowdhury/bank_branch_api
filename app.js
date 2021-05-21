61ae20ef1fbe448b32e163b9d38c7970c259f70d
Geety Ara Chowdhury <geetyarachowdhury@gmail.com>
2021-05-22 00:52:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bankRouter = require('./routes/banks');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.use("/api", bankRouter);
+app.use(cors());
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,17 @@
+// requir for creating server
+const http = require('http');
+const app = require('./app');
+
+// import app
+
+// port
+const port = process.env.PORT || 8080;
+
+// create a server with a func (which we define in app.js) is executed whenever we get a new request and get a response
+const server = http.createServer(app);
+
+// listen to port with server
+
+server.listen(port, () => {
+    console.log("server is listening to port:", port);
+});
