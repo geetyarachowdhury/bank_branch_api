044bcde38d1b4d59990df3166449c63bd7b79159
Geety Ara Chowdhury <geetyarachowdhury@gmail.com>
2021-05-22 14:08:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const bankRouter = require('./routes/banks');
 const cors = require('cors');
 
+const mySqlDb = require('../connection');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,7 +13,16 @@ app.use(bodyParser.json());
 
 app.use("/api", bankRouter);
 app.use(cors());
-
+app.get("/", cors(), (req, res) => {
+    mySqlDb.query("SELECT * from bank_branches", (err, rows, fields) => {
+        if(!err) {
+            console.log(rows.length)
+            res.json(rows);
+        } else {
+            console.log(err);
+        }
+    })
+})
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
