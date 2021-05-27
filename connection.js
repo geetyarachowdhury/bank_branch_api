c10216cd68b8f49f87c7e171096e308951f8e33b
Geety Ara Chowdhury <geetyarachowdhury@gmail.com>
2021-05-27 23:30:53 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,21 +1,13 @@
 const mysql = require('mysql');
 const dotenv = require('dotenv').config();
 
-let mySqlConnection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER, 
+let mySqlConnection = mysql.createPool({
+    host: process.env.DB_HOST ,
+    user: process.env.DB_USER , 
     password: process.env.DB_PASS,
-    database: "bankdb",
+    database: process.env.DB_NAME ,
     multipleStatements: true,
-    insecureAuth: false,
 });
 
-mySqlConnection.connect(err => {
-    if(!err) {
-        console.log("connected");
-    } else {
-        console.log(err);
-    }
-})
 
 module.exports = mySqlConnection;
